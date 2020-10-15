5e4641c93df2947d74332c2649ff9ebc126e57e1
José Nogueira <josej@bdnes.gov.br>
2020-10-15 13:13:21 -0300

    .

---
+++ contracts/0. presenca/Ateste.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.2;
+
+contract Ateste {
+    
+    mapping (address => string) public confirmados;
+    string[] public nomes;
+    
+    function atestar(string memory _nome) public {
+        address enderecoQueDesejaAtestar = msg.sender;
+        require( comparaString(confirmados[enderecoQueDesejaAtestar], "") , "Confirmacao ja realizada");
+        confirmados[enderecoQueDesejaAtestar] = _nome;
+        nomes.push(_nome);
+    }
+
+    function comparaString(string memory a, string memory b) private pure returns (bool) {
+        return keccak256(abi.encodePacked(a)) 
+                 == keccak256(abi.encodePacked(b));
+    }
+}
